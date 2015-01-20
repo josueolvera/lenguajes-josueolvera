4de5140c76f59647021b3b3462893898b71b162b
josueolvera <josue.olvera.r@gmail.com>
2015-01-19 19:33:30 -0600

    Update LenguajesJosueolvera.java

--- src/lenguajes/josueolvera/LenguajesJosueolvera.java
+++ src/lenguajes/josueolvera/LenguajesJosueolvera.java
@@ -15,7 +15,7 @@ public class LenguajesJosueolvera {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi primer programa de java II");  
+        System.out.println("Linea Modificada");  
       
         
     }
