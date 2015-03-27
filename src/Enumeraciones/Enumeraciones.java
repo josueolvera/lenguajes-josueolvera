a243741f4e18583693933f444639ca310a679f68
JOSUE <JOSUE@192.168.1.73>
2015-03-27 00:03:00 -0600

    @daton Buenas Noches profe ya quedo mi tarea

--- src/Enumeraciones/Enumeraciones.java
+++ src/Enumeraciones/Enumeraciones.java
@@ -18,6 +18,20 @@ public class Enumeraciones {
         MaquinaBuena maquina= new MaquinaBuena();
         maquina.setEstado(EstadoDeMaquina.ATASCADA);
         System.out.println("El estado de la maquina es:"+maquina.getEstado());
+        
+        
+        
+        Vaso v = new Vaso();
+        v.setTipo(TamañoVaso.MEDIANO);
+        System.out.println("El tamaño del vaso es:"+v.getTipo());
+        
+        Vaso b = new Vaso();
+        b.setTipo(TamañoVaso.CHICO);
+        System.out.println("El tamaño del vaso es:"+b.getTipo());
+        
+        Vaso a = new Vaso();
+        a.setTipo(TamañoVaso.GRANDE);
+        System.out.println("El tamaño del vaso es:"+a.getTipo());
     }
     
 }
---
+++ src/Enumeraciones/Vaso.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enumeraciones;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class Vaso {
+    private TamañoVaso tipo;
+
+    public TamañoVaso getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TamañoVaso tipo) {
+        this.tipo = tipo;
+    }
+}
