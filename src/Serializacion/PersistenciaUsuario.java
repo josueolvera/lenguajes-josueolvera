490647c25271d817384c5996ca2cbc77f43f3410
T-301 <T-301@PC335>
2015-03-26 19:43:30 -0600

    Enum

---
+++ src/Serializacion/PersistenciaUsuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serializacion;
+
+/**
+ *
+ * @author T-301
+ */
+public class PersistenciaUsuario {
+ 
+    public static void guardar(Cliente c)throws Exception{
+        //Aqui va logica muy loca pero a la vez hermosa 
+        
+    }
+    
+    public static Cliente leer(String nombre)throws Exception{
+        //Aqui tambn va codigo muy loco pero hermoso
+    }
+    
+}
